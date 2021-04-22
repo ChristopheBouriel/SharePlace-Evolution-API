3358d4f456fed67ab4276ef9249920665938e869
ChristopheBouriel <christophe.bouriel@gmail.com>
2021-04-22 21:09:32 +0200

    correction bodyParser deprecated

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const path = require('path');
 const cors = require('cors');
@@ -21,7 +21,7 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
